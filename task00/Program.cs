d6f7c7ec5380a9c3c90fcab093eb2d354503274b
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-15 19:43:27 +0300

    weekend task done

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите номер дня недели");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a==1 || a==2||a==3 || a==4 || a==5)
+Console.WriteLine("это будний день");
+else if (a==6 || a==7)
+Console.WriteLine("это выходной");
+else if (a>7)
+Console.WriteLine("в неделе 7 дней, введите число от 1 до 7");
