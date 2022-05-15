54ffab9b0bfe01b8057a1b400fb7b7f1d03f2b1a
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-15 22:35:48 +0300

    done

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите координату Х");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите координату Y");
+int y = int.Parse(Console.ReadLine() ?? "0");
+
+if (x>0 && y>0)
+Console.WriteLine ("I четверть! (x>0,y>0)");
+if (x<0 && y>0)
+Console.WriteLine ("II четверть! (x<0,y>0)");
+if (x<0 && y<0)
+Console.WriteLine ("III четверть! (x<0,y<0)");
+if (x>0 && y<0)
+Console.WriteLine ("IV четверть! (x>0,y<0)");
---
+++ task03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите координаты (x1,y1,z1) первой точки");
+int x1 = int.Parse(Console.ReadLine() ?? "0");
+int y1 = int.Parse(Console.ReadLine() ?? "0");
+int z1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("введите координаты (x2,y2,z2) второй точки");
+int x2 = int.Parse(Console.ReadLine() ?? "0");
+int y2 = int.Parse(Console.ReadLine() ?? "0");
+int z2 = int.Parse(Console.ReadLine() ?? "0");
+
+double a = Math.Sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1)));
+Console.WriteLine ("расстояние между точками:");
+Console.WriteLine (a);
