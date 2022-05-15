e21b85a87e553860c73570fca3a20b3f2655a2cb
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-15 20:13:22 +0300

    task square of the number done

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите два числа");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = int.Parse(Console.ReadLine() ?? "0");
+if (a==b*b || b==a*a)
+Console.WriteLine("одно из чисел является квадратом второго");
+else Console.WriteLine("одно из чисел не является квадратом второго");
