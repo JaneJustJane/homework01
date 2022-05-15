c7d59b176bd1c0142451bdb740c4305d6921f557
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-15 23:12:24 +0300

    the 2nd way of the 2nd task

---
+++ task02new/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите номер плоскости");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a==1)
+Console.WriteLine ("I четверть! (x>0,y>0)");
+if (a==2)
+Console.WriteLine ("II четверть! (x<0,y>0)");
+if (a==3)
+Console.WriteLine ("III четверть! (x<0,y<0)");
+if (a==4)
+Console.WriteLine ("IV четверть! (x>0,y<0)");
+if (a<=0 || a>4)
+Console.WriteLine ("Введите номер от 1 до 4!");
